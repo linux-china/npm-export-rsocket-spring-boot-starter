fd37a9108d4b8d64309920d6d3336d7d3de222f0
linux_china <libing.chen@gmail.com>
2020-04-01 14:51:41 -0700

    RSocket Boot demo app

---
+++ src/test/java/org/mvnsearch/boot/npm/export/demo/RSocketBootDemoApp.java
@@ -0,0 +1,16 @@
+package org.mvnsearch.boot.npm.export.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * RSocket Boot demo app
+ *
+ * @author linux_china
+ */
+@SpringBootApplication
+public class RSocketBootDemoApp {
+    public static void main(String[] args) {
+        SpringApplication.run(RSocketBootDemoApp.class, args);
+    }
+}
