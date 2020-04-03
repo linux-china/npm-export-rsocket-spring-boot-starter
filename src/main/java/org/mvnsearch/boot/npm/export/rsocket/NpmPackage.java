50349de8dc46f884670b3ae76e85535427685f80
linux_china <libing.chen@gmail.com>
2020-04-03 12:15:48 -0700

    NpmPackage annotation

---
+++ src/main/java/org/mvnsearch/boot/npm/export/rsocket/NpmPackage.java
@@ -0,0 +1,18 @@
+package org.mvnsearch.boot.npm.export.rsocket;
+
+import java.lang.annotation.*;
+
+/**
+ * npm package annotation
+ *
+ * @author linux_china
+ */
+@Target(ElementType.TYPE)
+@Retention(RetentionPolicy.RUNTIME)
+@Documented
+public @interface NpmPackage {
+
+    String value() default "";
+
+    String version() default "";
+}
